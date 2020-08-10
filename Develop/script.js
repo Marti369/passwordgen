58687cdf2ed063df9e88e65c7854baf025bb1e8b
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-08-09 18:39:20 -0700

    add semicolum to project

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,29 @@
 // this declares the variables and id selectors from html
-var characterAmountRange = document.getElementById('characterAmountRange')
-var characterAmountNumber = document.getElementById('characterAmountNumber')
+var characterAmountRange = document.getElementById('characterAmountRange');
+var characterAmountNumber = document.getElementById('characterAmountNumber');
 
-var includeUppercaseElement = document.getElementById('includeUppercase')
-var includeNumbersElement = document.getElementById('includeNumbers')
-var includeSymbolsElement = document.getElementById('includeSymbols')
-var form = document.getElementById('passwordGeneratorForm')
-var passwordDisplay = document.getElementById('passwordDisplay')
+var includeUppercaseElement = document.getElementById('includeUppercase');
+var includeNumbersElement = document.getElementById('includeNumbers');
+var includeSymbolsElement = document.getElementById('includeSymbols');
+var form = document.getElementById('passwordGeneratorForm');
+var passwordDisplay = document.getElementById('passwordDisplay');
 
 //this array delcares all possible characters for our function
-var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-var NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+var NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
 var SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
     arrayFromLowToHigh(58, 64)
 ).concat(
     arrayFromLowToHigh(91, 96)
 ).concat(
     arrayFromLowToHigh(123, 126)
-)
+);
 
 
 //this will link when we change the slider the number box changes
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
+characterAmountNumber.addEventListener('input', syncCharacterAmount);
+characterAmountRange.addEventListener('input', syncCharacterAmount);
 
 form.addEventListener('submit', e => {
     e.preventDefault() //this prevents the default 
@@ -34,21 +34,21 @@ form.addEventListener('submit', e => {
         //here we tell the function what to generate if we choose options
     var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
     passwordDisplay.innerText = password
-})
+});
 
 //function to generate password
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-    let charCodes = LOWERCASE_CHAR_CODES
-    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    let charCodes = LOWERCASE_CHAR_CODES;
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
 
     var passwordCharacters = []
     for (let i = 0; i < characterAmount; i++) {
         var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
         passwordCharacters.push(String.fromCharCode(characterCode))
-    }
-    return passwordCharacters.join('')
+    };
+    return passwordCharacters.join('');
 }
 
 
@@ -58,12 +58,12 @@ function arrayFromLowToHigh(low, high) {
     for (let i = low; i <= high; i++) {
         array.push(i)
     }
-    return array
+    return array;
 }
 
 
 function syncCharacterAmount(e) {
     var value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
+    characterAmountNumber.value = value;
+    characterAmountRange.value = value;
 }
